d64895bfbce267f81d092eb2667cef898ecf80f4
Praneet Masiya <praneetmasiya23@gmail.com>
2025-07-17 20:10:03 +0530

    ,

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,5 +1,4 @@
-import { getAuth } from "@clerk/nextjs/dist/types/server";
-import { getAppPageStaticInfo } from "next/dist/build/analysis/get-page-static-info";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Product from "@/models/Product";
 import { inngest } from "@/config/inngest";
